9f1d1a39cce80e3131d6f0133ff5a6d033aaf6a3
Bruno Coimbra <57022639+brncoimbra@users.noreply.github.com>
2023-01-30 19:12:22 -0300

    create pokecount components

---
+++ src/components/PokemonCount/atoms.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { FlexBox } from "../Flexbox";
+
+export const PokemonCountContainer = styled(FlexBox)`
+  font-weight: 600;
+  font-size: 1.25em;
+`
+
---
+++ src/components/PokemonCount/index.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+
+import * as Atom from "./atoms";
+import { MdCatchingPokemon } from "react-icons/md";
+
+export interface IPokemonCount {
+  count: number;
+}
+
+const PokemonCount: FC<IPokemonCount> = ({ count }) => {
+  return (
+    <Atom.PokemonCountContainer
+      align='center'
+      justify='flex-start'
+      direction='row'
+      gap='xxs'
+      wrap='wrap'
+    >
+      <MdCatchingPokemon size='20' />
+      {count} Pokemons
+    </Atom.PokemonCountContainer>
+  );
+};
+
+export default PokemonCount;
